35a8e6daf6c14e2ea2d52b0cd89d6273ae9639eb
Wes Garrison <wes@databasically.com>
2023-02-05 21:55:10 -0600

    initial commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    scrollRestoration: true,
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
+  }
+}
+
+module.exports = nextConfig
